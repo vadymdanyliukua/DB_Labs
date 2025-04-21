2dd1b8fe86442190d8b856c768ecebe2daa620ef
PysJlaH_JlT <ruslan160306@gmail.com>
2025-04-21 12:49:19 +0300

    Дороблені деякі зміни

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,26 +2,17 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "rrgrgrgrg",
+  title: "Проєкт студентів ФІОТу",
   description: "Lab5",
   base:"/Develop1/",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: 'Zmist', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' },
-      {text:'Authors', link: '/authors' },
+      { text: 'Автори', link: '/authors' },
+      { text: 'Аналіз предметної області', link: '/vstup' },
+      {text:'Запити зацікавлених осіб', link: '/zapity' },
     ],
 
-    sidebar: [
-      {
-        text: 'Examples',
-        items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
-    ],
 
     socialLinks: [
       { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
