311941bb8a0e35d7cc7a34f162f5970c5c248d4d
Tymur Varshavskiy <74325939+varshavskiy05@users.noreply.github.com>
2025-04-21 17:56:45 +0300

    Update config.mjs

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "Проєкт студентів ФІОТу",
   description: "Lab5",
-  base:"/Develop1/",
+  base:"/DB_Labs/",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
@@ -15,7 +15,7 @@ export default defineConfig({
 
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+      { icon: 'github', link: 'https://github.com/Ruslan-LT/DB_Labs.git' }
     ]
   }
 })
